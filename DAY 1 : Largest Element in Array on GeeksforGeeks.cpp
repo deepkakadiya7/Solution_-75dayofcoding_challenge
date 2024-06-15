a098a22b3356ba1c41eebe72d2dae16ae890749a
DEEP KAKADIYA <142491093+deepkakadiya7@users.noreply.github.com>
2024-06-15 12:51:03 +0530

    Update DAY 1 : Largest Element in Array on GeeksforGeeks.cpp

--- DAY 1 : Largest Element in Array on GeeksforGeeks.cpp
+++ DAY 1 : Largest Element in Array on GeeksforGeeks.cpp
@@ -1,1 +1,40 @@
-# Solution_-75dayofcoding_challenge
+//**Solution 1 :Brute Force Approach 
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+    sort(arr.begin(),arr.end());
+    return arr[arr.size()-1];
+
+    }
+};
+
+// Complexity Analysis
+// Time Complexity: O(N*log(N))
+
+// Space Complexity: O(n)
+
+//**Solution 2 : Recursive Approach
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int larg=arr[0];
+        for(int i =0;i<n;i++){
+            
+            if(arr[i]>larg){
+                larg=arr[i];
+            }
+        }
+            return larg;
+    }
+};
+
+// Complexity Analysis
+// Time Complexity: O(N)
+
+// Space Complexity: O(1)
