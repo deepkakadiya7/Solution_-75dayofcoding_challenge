04ed6c5c75fd34561d4764acfcb1e17387e248a8
DEEP KAKADIYA <142491093+deepkakadiya7@users.noreply.github.com>
2024-06-16 11:55:43 +0530

    Update DAY 2 :Find Second Largest Element in an array GeeksforGeeks.cpp

--- DAY 2 :Find Second Largest Element in an array GeeksforGeeks.cpp
+++ DAY 2 :Find Second Largest Element in an array GeeksforGeeks.cpp
@@ -1,27 +1,20 @@
-class Solution{
+class Solution {
 public:	
-	// Function returns the second
-	// largest elements
-	int print2largest(int arr[], int n) {
-	     if(n<2){
-            return -1;
+    // Function returns the second largest element
+    int print2largest(int arr[], int n) {
+        if (n < 2) return -1;
+        
+        int largest = INT_MIN, second_largest = INT_MIN;
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                second_largest = largest;
+                largest = arr[i];
+            } else if (arr[i] > second_largest && arr[i] != largest) {
+                second_largest = arr[i];
+            }
         }
         
-        int i,largest=arr[0];
-        int slargest=-1;
-         for (i=0; i<n; i++)
-         {
-             if(arr[i]>largest)
-             largest=arr[i];
-         
-         }
-          
-         for(i=0; i<n; i++)
-         {
-             if(arr[i]>slargest && arr[i]!=largest)
-             slargest=arr[i];
-         
-         }
-         return slargest;
-	}
+        return second_largest == INT_MIN ? -1 : second_largest;
+    }
 };
