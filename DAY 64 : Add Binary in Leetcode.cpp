1e0bb56d5b2e02202c71d5cd902fe713b6b2c21c
DEEP KAKADIYA <142491093+deepkakadiya7@users.noreply.github.com>
2024-08-18 19:33:14 +0530

    Create DAY 64 : Add Binary in Leetcode.cpp

---
+++ DAY 64 : Add Binary in Leetcode.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    string addBinary(string a, string b)
+    {
+        string str;
+        int m = a.length() - 1;
+        int n = b.length() - 1;
+        int carry = 0;
+
+
+        while (m >= 0 || n >= 0)
+        {
+            int sum = carry;
+            if (m >= 0)
+                sum += a[m--] - '0';
+            if (n >= 0)
+                sum += b[n--] - '0';
+            carry = sum > 1 ? 1 : 0;
+            str += to_string(sum % 2);
+        }
+        if (carry)
+            str += to_string(carry);
+        reverse(str.begin(), str.end());
+        return str;
+    }
+};
